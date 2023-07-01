8af67146db28e4f8752543940c94610154b8ef7d
KenzoUMZ <kenzoumezawa@gmail.com>
2023-06-30 22:45:11 -0300

    lab rest pt1 and pt2

---
+++ workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/LabRestServerApplication.java
@@ -0,0 +1,16 @@
+package br.inatel.labs.labrest.server;
+
+ * @ 06.05.2023
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabRestServerApplication.class, args);
+	}
+
+}
---
+++ workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/controller/HelloController.java
@@ -0,0 +1,21 @@
+package br.inatel.labs.labrest.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ * @ 06.05.2023
+ */
+
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+	@GetMapping
+	public MyMessage processarGetHello() {
+		MyMessage msg = new MyMessage();
+		msg.setInfo("Olá mundo REST");
+		return msg;
+	}
+}
